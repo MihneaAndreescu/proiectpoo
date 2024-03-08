6ad04a2593046429d1913f138bd35a0d1af48a93
MihneaAndreescu <84676233+MihneaAndreescu@users.noreply.github.com>
2024-03-08 23:24:06 +0200

    moved to main

--- main.cpp
+++ main.cpp
@@ -111,84 +111,31 @@ void GO()
 
 int main()
 {
-    GO();
-
-    exit(0);
-    //exit(0);
-    ////////////////////////////////////////////////////////////////////////
-    /// NOTE: this function call is needed for environment-specific fixes //
-    init_threads();                                                       //
-    ////////////////////////////////////////////////////////////////////////
-    ///
-    //std::cout << "Hello, world!\n";
-    //std::array<int, 100> v{};
-    //int nr;
-    //std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    //std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    //for(int i = 0; i < nr; ++i) {
-    //    std::cout << "v[" << i << "] = ";
-    //    std::cin >> v[i];
-    //}
-    //std::cout << "\n\n";
-    //std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    //for(int i = 0; i < nr; ++i) {
-    //    std::cout << "- " << v[i] << "\n";
-    //}
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod generat                     ///
-    ///////////////////////////////////////////////////////////////////////////
+    Planet dune{ "Dune", sf::Vector2f(0.4f, 0.6f), 0.2f };
+    
+    
+    init_threads();                                                      
     Helper helper;
     helper.help();
-    ///////////////////////////////////////////////////////////////////////////
 
     SomeClass* c = getC();
     std::cout << c << "\n";
     delete c;
 
     sf::RenderWindow window;
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: sync with env variable APP_WINDOW from .github/workflows/cmake.yml:31
-    window.create(sf::VideoMode({ 800, 700 }), "My Window", sf::Style::Default);
-    ///////////////////////////////////////////////////////////////////////////
-    //
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: mandatory use one of vsync or FPS limit (not both)            ///
-    /// This is needed so we do not burn the GPU                            ///
-    window.setVerticalSyncEnabled(true);                            ///
-    /// window.setFramerateLimit(60);                                       ///
-    ///////////////////////////////////////////////////////////////////////////
+    window.create(sf::VideoMode({ 900, 900 }), "Muad'dib", sf::Style::Default);
+    window.setVerticalSyncEnabled(true);                      
+    
+    sf::View view;
+    view.setSize(sf::Vector2f(1, 1));
+    view.setCenter(sf::Vector2f(0.5, 0.5));
+    window.setView(view);
 
-    //exit(0);
-    while (window.isOpen()) {
+    sf::Clock secondClock;
+    int fps = 0;
+
+    while (window.isOpen()) 
+    {
         bool shouldExit = false;
         sf::Event e{};
         while (window.pollEvent(e)) {
@@ -213,10 +160,21 @@ int main()
             window.close();
             break;
         }
-        using namespace std::chrono_literals;
-        std::this_thread::sleep_for(300ms);
+
+
+       
+        if (secondClock.getElapsedTime().asSeconds() >= 1)
+        {
+            std::cout << "fps = " << fps << std::endl;
+            secondClock.restart();
+            fps = 0;
+        }
+        fps++;
+
+
 
         window.clear();
+        dune.display(window);
         window.display();
     }
     return 0;
